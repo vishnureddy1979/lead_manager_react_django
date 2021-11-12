36238ee2e7472f4d95fd92037f2074faab3bc2b9
Vishnu <vishnu.reddy28@gmail.com>
2021-11-12 17:02:19 -0500

    first commit

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 't7g$gddxotz%5_gwrt64xf#-bjwr(bk_#10!ur9et_s-*vhqti'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
